b3af7dcdeef6af13cad4091af01043f4bc363338
Miles Mao <mrmao1@gmail.com>
2022-12-30 12:29:49 -0600

    selection statements exercise 2 results

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,28 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("What is your favorite school subject?");
+
+            var subject = Console.ReadLine();
+
+            switch (subject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is a challenging subject!");
+                    break;
+                case "science":
+                    Console.WriteLine("Science is interesting!");
+                    break;
+                case "english":
+                    Console.WriteLine("English is an expressive subject!");
+                    break;
+                default:
+                    Console.WriteLine("Oh Wow! I never took that subject.");
+                    break;
+
+
+            }
+            
         }
     }
 }
